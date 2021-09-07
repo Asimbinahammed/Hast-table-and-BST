6142833db1b660e1f069fb09e3b49e960758a03a
asimbinahammed <asim242370@gmail.com>
2021-09-07 11:40:06 +0530

    Added more elements and check the count

--- BSTmain.java
+++ BSTmain.java
@@ -10,9 +10,23 @@ public class BSTmain {
 		obj.insert(30);
 		obj.insert(70);
 		
-		//displaying
-		System.out.println("Data in the format of left-root-right");
+		//for uc2
+		obj.insert(22);
+		obj.insert(40);
+		obj.insert(11);
+		obj.insert(16);
+		obj.insert(3);
+		obj.insert(70);
+		obj.insert(95);
+		obj.insert(60);
+		obj.insert(65);
+		obj.insert(63);
+		obj.insert(67);
+		
+		//displaying in inorder method
+		System.out.println("Data in the format of ascending order");
 		obj.inorder();
+		
 	}
 
 }
