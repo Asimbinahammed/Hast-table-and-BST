701c00b2166f48a6f9b549f692d2579878b53620
asimbinahammed <asim242370@gmail.com>
2021-09-09 22:28:31 +0530

    Removing avoidable

--- HashTableMain.java
+++ HashTableMain.java
@@ -2,23 +2,31 @@ package bridgelabzz;
 
 public class HashTableMain {
 	public static void main(String[] args) {
-		HashTableImplementation<String, Integer> hashImpl = new HashTableImplementation<String, Integer>();
-		String message = "To be or not to be";
+		HashTableImplementation<String, Integer> hashImpl = new HashTableImplementation();
+		
+		//input data
+		String message = "Paranoids are not paranoid because they are paranoid but " +
+                "because they keep putting themselves deliberately into paranoid avoidable situations";
 
-		// Split the words when a white space comes up and Convert all the words in lowercase
-		String[] messageArray = message.toLowerCase().split(" ");
+        //Split the words when a white space comes up and Convert all the words in lowercase
+        String[] messageArray = message.toLowerCase().split(" ");
 
-		for (String word : messageArray) {
-			// Search for the word in the linked list
-			Integer value = hashImpl.get(word);
-			// If not found, frequency of the word StayWith 1
-			if (value == null)
-				value = 1;
-			else
-				// If found, frequency of the word increases by 1
-				value = value + 1;
-			hashImpl.add(word, value);
-		}
-		System.out.println(hashImpl);
-	}
+        for(String word: messageArray) {
+            //Search for the word in the hashtable
+            Integer value =  hashImpl.get(word);
+            //If not found, frequency of the word StayWith 1
+            if( value == null)
+                value = 1;
+            else
+                //If found, frequency of the word increases by 1
+                value = value + 1;
+            hashImpl.add(word , value);
+        }
+
+        //Remove "avoidable" from the hashtable
+        hashImpl.remove("avoidable");
+
+        //Display the hashtable
+        System.out.println(hashImpl);
+    }
 }
